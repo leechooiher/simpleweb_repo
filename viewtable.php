843761a28ff560c001d44b73129dfbabbc62c574
lch <leechooiher2@gmail.com>
2021-02-08 23:54:03 +0800

    add reading script

--- viewtable.php
+++ viewtable.php
@@ -29,7 +29,25 @@
                 } else {
                     echo "<script>alert('Success!');</script>";
                 }
+
+                $sql= "SELECT * FROM [dbo].[restaurant]";
+                $getResults= sqlsrv_query($conn, $sql);
+
+                if ($getResults == FALSE)
+                {
+                die(sqlsrv_errors());
+                }
                 
+                while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC))
+                {
+                echo "<tr>";
+                echo "<td>". $row['restaurant_id'] . "</td>";
+                echo "<td>". $row['restaurant_name'] ."</td>";
+                echo "<td>". $row['restaurant_address'] . "</td>";
+                echo "<td>". $row['restaurant_phone'] . "</td>";
+                echo "</tr>";
+                }
+                sqlsrv_free_stmt($getResults);
             ?>
         </table>
         </center>
